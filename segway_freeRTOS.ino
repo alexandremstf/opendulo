cadb98e5d676c5a5d01ce5de10a1b7ab9c8a9955
Paulo Santos <paulosantosphs@hotmail.com>
2018-12-12 16:50:30 -0200

    reading

--- segway_freeRTOS.ino
+++ segway_freeRTOS.ino
@@ -1,5 +1,23 @@
 #include <Arduino_FreeRTOS.h>
+#include "SparkFunMPU9250-DMP.h"
+#include "I2Cdev.h"
+#include "Kalman.h"
 
+// MPU9250
+MPU9250_DMP imu_9250;
+float accelX, accelY, accelZ;
+float gyroX, gyroY, gyroZ;
+float roll, pitch;
+float gyroXrate, gyroYrate;
+float rad_to_reg = 180 / 3.141592654;
+
+// Kalman Filter
+Kalman kalmanX;
+Kalman kalmanY;
+double gyroXangle, gyroYangle; // Gyroscope angle
+double compAngleX, compAngleY; // Complementary filter angle
+double kalAngleX, kalAngleY; // Angle after Kalman filter
+double corrected_x, corrected_y; // Corrected with offset
 const byte AIN2 = 4;
 const byte AIN1 = 5;
 const byte STBY = 6;
@@ -7,6 +25,96 @@ const byte BIN1 = 7;
 const byte BIN2 = 8;
 const byte PWM_MOTOR1 = 9;
 const byte PWM_MOTOR2 = 10;
+
+// Timer
+float now_time;
+float pas_time;
+float dif_time;
+
+// PID
+float kp = 22;// 10.1 18
+float ki = 0.4;// 0.3
+float kd = 20;// 9.3 16
+float kp_error = 0.0;
+float ki_error = 0.0;
+float kd_error = 0.0;
+float kp_pass_error = 0.0;
+float kp_result = 0;
+float ki_result = 0;
+float kd_result = 0;
+float final_result = 0;
+
+// Special angle
+float overshoot_angle = 30;
+float PID_angle = 8;
+float reference_angle = 0.0;
+
+void UpdateIMUData(void)
+{
+  accelX = imu_9250.calcAccel(imu_9250.ax);
+  accelY = imu_9250.calcAccel(imu_9250.ay);
+  accelZ = imu_9250.calcAccel(imu_9250.az);
+  gyroX = imu_9250.calcGyro(imu_9250.gx);
+  gyroY = imu_9250.calcGyro(imu_9250.gy);
+  gyroZ = imu_9250.calcGyro(imu_9250.gz);
+
+  // Convert to deg/s
+  roll = atan(accelY / sqrt(pow(accelX, 2) + pow(accelZ, 2))) * rad_to_reg;
+  pitch = atan(-1 * accelX / sqrt(pow(accelY, 2) + pow(accelZ, 2))) * rad_to_reg;
+  gyroXrate = gyroX / 131.0;
+  gyroYrate = gyroY / 131.0;
+}
+
+float pid_control() { // ONLY PD RIGHT NOW
+  kp_error = kalAngleY - reference_angle;
+
+  // If the car is about to fall down, adjust it quickly
+  if (kp_error >= overshoot_angle && kp_error <= -overshoot_angle) {kp = 40;}
+  else {kp = 22;}
+
+  ki_error += kp_error * dif_time;
+  kd_error = (kp_error - kp_pass_error) / dif_time;
+  kp_result = kp_error * kp;
+  ki_result = ki_error * ki;
+  kd_result = kd_error * kd;
+  kp_pass_error = kp_error;
+  final_result = kp_result + kd_result;
+
+  // PID only invoided when angle is small
+  if (kp_error <= PID_angle && kp_error >= -PID_angle) {
+    final_result = kp_result + kd_result + ki_result;
+  }
+
+  return final_result;
+}
+
+void kalman() {
+  if ((roll < -90 && kalAngleX > 90) || (roll > 90 && kalAngleX < -90)) {
+    kalmanX.setAngle(roll);
+    compAngleX = roll;
+    kalAngleX = roll;
+    gyroXangle = roll;
+  } else {
+    kalAngleX = kalmanX.getAngle(roll, gyroXrate, dif_time); // Calculate the angle using a Kalman filter
+  }
+  if (abs(kalAngleX) > 90) {
+    gyroYrate = -gyroYrate; // Invert rate, so it fits the restriced accelerometer reading
+  }
+  kalAngleY = kalmanY.getAngle(pitch, gyroYrate, dif_time);
+  gyroXangle += gyroXrate * dif_time; // Calculate gyro angle without any filter
+  gyroYangle += gyroYrate * dif_time;
+  
+  Serial.print("X Angle: ");
+  Serial.print(kalAngleX);
+  Serial.print(" Y Angle: ");
+  Serial.println(kalAngleY);  
+  
+  // Reset the gyro angle when it has drifted too much
+  if (gyroXangle < -180 || gyroXangle > 180)
+    gyroXangle = kalAngleX;
+  if (gyroYangle < -180 || gyroYangle > 180)
+    gyroYangle = kalAngleY;
+}
 
 int pwm1 = 0;
 int pwm2 = 0;
@@ -24,10 +132,34 @@ void setup() {
 
   xTaskCreate(control, (const portCHAR *)"Control", 128, NULL, 2, NULL );
   xTaskCreate(readSensor, (const portCHAR *)"Control", 128, NULL, 1, NULL );
+
+  // MPU-9250
+  if (imu_9250.begin() != INV_SUCCESS)
+  {
+    imu_9250.setSensors(INV_XYZ_GYRO | INV_XYZ_ACCEL);// Enable all sensors:
+    imu_9250.setGyroFSR(2000); // Set gyro to 2000 dps
+    imu_9250.setAccelFSR(2); // Set accel to +/-2g
+    imu_9250.setLPF(5); // Set LPF corner frequency to 5Hz
+    imu_9250.setSampleRate(10); // Set sample rate to 10Hz
+  }
+
+  // Timer
+  pas_time = millis();
 }
 
 void loop() {
-  // Empty. Things are done in Tasks.
+  // calculate time
+  now_time = millis();
+  dif_time = (now_time - pas_time) / 1000; // in seconds. We work in ms so we haveto divide the value by 1000
+  pas_time = now_time;
+
+  // Update IMU data
+  if ( imu_9250.dataReady() )
+  {
+    imu_9250.update(UPDATE_ACCEL | UPDATE_GYRO | UPDATE_COMPASS);
+    UpdateIMUData();
+    kalman();
+  }
 }
 
 void control(void *pvParameters) {
